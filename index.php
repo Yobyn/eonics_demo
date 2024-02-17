a8022be37c67c46cfef6aea30ac884b06a065ff0
Yobyn Roetz <yroetz@gmail.com>
2024-02-17 22:17:50 +0100

    connect to database

--- index.php
+++ index.php
@@ -1,3 +1,20 @@
 <?php
-echo "Hello all";
+$servername = "localhost";
+$username = "yobynrur_demo";
+$password = "6,9g[eM89hb{";
+$database = "yobynrur_demo";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $database);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+echo "Connected successfully";
+
+
+
+
 ?>
